3dd35a6c7a093b930a6a9c4999d07c6c99b69248
Jakob Skelmose <jakob1741@hotmail.com>
2026-06-18 08:41:36 +0200

    Add session statistics and renaming features

--- vibe_viewer/__init__.py
+++ vibe_viewer/__init__.py
@@ -5,7 +5,7 @@ A clean Python package that provides a web interface to view your
 Mistral Vibe CLI conversation history in a browser.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Jakob"
 __description__ = "Web viewer for Mistral Vibe CLI conversations"
 
